ae8ea7557ea6e911419787418aeb648411d00103
rdnunezal <rdnunezal@gmail.com>
2025-01-31 02:25:57 +0000

    add datos-usuarios.py file to Curso-Entrenamiento-CCNA-DEVNET

---
+++ datos-usuarios.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+sede = input("Ingrese su sede: ")
+
+print(f"Datos ingresados:\nNombre: {nombre} {apellido}\nEdad: {edad}\nSede: {sede}")
